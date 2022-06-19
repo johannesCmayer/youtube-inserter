6ad274df3b16cfb4845718973f350792f375aa6b
Johannes C. Mayer <j.c.mayer240@gmail.com>
2022-06-19 23:16:33 +0200

    Update youtube-inserter.py

--- youtube-inserter.py
+++ youtube-inserter.py
@@ -1,9 +1,3 @@
-# -*- coding: utf-8 -*-
-
-# Sample Python code for youtube.playlistItems.insert
-# See instructions for running these code samples locally:
-# https://developers.google.com/explorer-help/code-samples#python
-
 import os
 import sys
 import pickle
